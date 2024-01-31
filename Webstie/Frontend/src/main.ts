4ee539940a4d8e103b3d43cc9e4f4da110f33127
freekesser <freek.v.esser@gmail.com>
2024-01-31 14:56:34 +0100

    frontend working

--- Webstie/Frontend/src/main.ts
+++ Webstie/Frontend/src/main.ts
@@ -2,6 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from 'axios';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
